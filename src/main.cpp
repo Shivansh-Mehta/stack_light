150b18a7738f9d74d45154c4626b6235781b9dee
Shivansh-Mehta <shivansh.mehta.1211@gmail.com>
2026-03-12 15:57:10 +0100

    microros integration

--- src/main.cpp
+++ src/main.cpp
@@ -1,15 +1,15 @@
 #include <Arduino.h>
+#include <micro_ros_platformio.h>
+#include <rcl/rcl.h>
+#include <rclc/rclc.h>
+#include <rclc/executor.h>
+#include <std_msgs/msg/string.h>
 
 #define RED_PIN PIN_A4    // pin 18
 #define YELLOW_PIN PIN_A5 // pin 19
 #define GREEN_PIN PIN_A8  // pin 22
 
-bool bRedPinFlag = false;
-unsigned long periodMicro = 500000;
-unsigned long refTime = 0;
-uint8_t color = 0;
-
-enum stackLightColor
+enum stack_light_color
 {
   red = 1,
   yellow,
@@ -17,12 +17,22 @@ enum stackLightColor
   disable,
 };
 
-void getUserInput();
-void setUserInput();
+int *color = nullptr;
+
+rclc_support_t support;
+rcl_node_t node;
+rcl_subscription_t subscriber;
+rclc_executor_t executor;
+std_msgs__msg__String i_stack_light_cmd_input;
+rcl_allocator_t allocator;
+
+void stack_light_callback(const void *i_stack_light_cmd_input);
 
 void setup()
 {
   Serial.begin(115200);
+  // USB serial comm setup
+  set_microros_serial_transports(Serial);
 
   // setting pins as output and at logic level low
   pinMode(RED_PIN, OUTPUT);
@@ -31,95 +41,66 @@ void setup()
   digitalWrite(YELLOW_PIN, LOW);
   pinMode(GREEN_PIN, OUTPUT);
   digitalWrite(GREEN_PIN, LOW);
+
+  // memory allocator
+  allocator = rcl_get_default_allocator();
+
+  // support with ros engine
+  rclc_support_init(&support, 0, NULL, &allocator);
+
+  // node setup
+  rclc_node_init_default(&node, "stacklight_node", "", &support);
+
+  rclc_subscription_init_default(&subscriber,
+                                 &node,
+                                 ROSIDL_GET_MSG_TYPE_SUPPORT(std_msgs, msg, String),
+                                 "stacklight_topic");
+
+  // main event loop
+  rclc_executor_init(&executor,
+                     &support.context,
+                     1,
+                     &allocator);
+
+  // adding the subscription via the executor to get inputs for configuring stack light
+  rclc_executor_add_subscription(&executor,
+                                 &subscriber,
+                                 &i_stack_light_cmd_input,
+                                 &stack_light_callback,
+                                 ON_NEW_DATA);
 }
 
 void loop()
 {
-  getUserInput();
-  setUserInput();
+  rclc_executor_spin_some(&executor, RCL_MS_TO_NS(10));
 }
 
-void getUserInput()
+void stack_light_callback(const void *i_stack_light_cmd_input)
 {
+  const std_msgs__msg__String *stack_light_cmd_input = (const std_msgs__msg__String *)i_stack_light_cmd_input;
+  if (NULL == stack_light_cmd_input)
+    return;
 
-  uint8_t localColor = 0;
+  digitalWrite(RED_PIN, LOW);
+  digitalWrite(YELLOW_PIN, LOW);
+  digitalWrite(GREEN_PIN, LOW);
 
-  while (!Serial)
-    ;
-  Serial.println("Enter:");
-  Serial.println("1\t:for red light");
-  Serial.println("2\t:for yellow light");
-  Serial.println("3\t:for green light");
-  Serial.println("4\t:for disabling the stacking light");
-  Serial.flush();
+  sscanf(stack_light_cmd_input->data.data, "%d", color);
+  if (nullptr == color)
+    return;
 
-  while (Serial.available() == 0)
-    ;
-  localColor = Serial.parseInt();
-  if (stackLightColor::red == localColor)
+  switch (*color)
   {
-    color = localColor;
-    Serial.println("Color set: red");
-    Serial.flush();
-  }
-  else if (stackLightColor::yellow == localColor)
-  {
-    color = localColor;
-    Serial.println("Color set: yellow");
-    Serial.flush();
-  }
-  else if (stackLightColor::green == localColor)
-  {
-    color = localColor;
-    Serial.println("Color set: green");
-    Serial.flush();
-  }
-  else if (stackLightColor::disable == localColor)
-  {
-    color = localColor;
-    Serial.println("Stacking light disabled");
-    Serial.flush();
-  }
-  else if (0 != localColor)
-  {
-    Serial.println("Incorrect input. Try again.");
-    Serial.flush();
-    getUserInput();
-  }
-  return;
-}
-
-void setUserInput()
-{
-  unsigned long updTime = micros();
-  while ((updTime - refTime) >= (periodMicro))
-  {
-    refTime = updTime;
-
-    digitalWrite(RED_PIN, LOW);
-    digitalWrite(YELLOW_PIN, LOW);
-    digitalWrite(GREEN_PIN, LOW);
-
-    switch (color)
-    {
-    case stackLightColor::red:
-      digitalWrite(RED_PIN, HIGH);
-      break;
-    case stackLightColor::yellow:
-      digitalWrite(YELLOW_PIN, HIGH);
-      break;
-    case stackLightColor::green:
-      digitalWrite(GREEN_PIN, HIGH);
-      break;
-    case stackLightColor::disable:
-    {
-      digitalWrite(RED_PIN, LOW);
-      digitalWrite(YELLOW_PIN, LOW);
-      digitalWrite(GREEN_PIN, LOW);
-    }
+  case stack_light_color::red:
+    digitalWrite(RED_PIN, HIGH);
     break;
-    }
+  case stack_light_color::yellow:
+    digitalWrite(YELLOW_PIN, HIGH);
+    break;
+  case stack_light_color::green:
+    digitalWrite(GREEN_PIN, HIGH);
+    break;
+  case stack_light_color::disable:
+    break;
   }
-
-  return;
 }
